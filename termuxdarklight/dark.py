2c1613ab4b7265bff49a97a59259c133d46f4a49
xthan <hackattendant@yahoo.com>
2026-06-15 15:53:08 -0400

    fix font path

--- termuxdarklight/dark.py
+++ termuxdarklight/dark.py
@@ -2,6 +2,6 @@ import os
 
 os.system("cat ~/.termux/termuxdarklight/dark/termux.properties > ~/.termux/termux.properties")
 os.system("cat ~/.termux/termuxdarklight/dark/colors.properties > ~/.termux/colors.properties")
-os.system("rm ~/.termux/font.ttf && cp ~/0/Tasker/termuxdarklight/dark/font.ttf ~/.termux/.")
+os.system("rm ~/.termux/font.ttf && cp ~/.termux/termuxdarklight/dark/font.ttf ~/.termux/.")
 os.system("termux-reload-settings")
 
--- termuxdarklight/light.py
+++ termuxdarklight/light.py
@@ -2,6 +2,6 @@ import os
 
 os.system("cat ~/.termux/termuxdarklight/light/termux.properties > ~/.termux/termux.properties")
 os.system("cat ~/.termux/termuxdarklight/light/colors.properties > ~/.termux/colors.properties")
-os.system("rm ~/.termux/font.ttf && cp ~/0/Tasker/termuxdarklight/light/font.ttf ~/.termux/.")
+os.system("rm ~/.termux/font.ttf && cp ~/.termux/termuxdarklight/light/font.ttf ~/.termux/.")
 os.system("termux-reload-settings")
 
